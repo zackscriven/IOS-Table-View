37df7e5b77c9d314b712a80e6ecc276ee99f193a
Archetapp <archetapp@gmail.com>
2015-01-16 16:35:18 -0700

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Jared Davidson on 1/16/15.
+//  Copyright (c) 2015 Archetapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
